2a67e5896cd2935a6b8530b9b65f8cdd62c98102
Kutuzov Dmitry <archie-woc@ya.ru>
2025-03-26 22:15:24 +0300

    Initial setup of spring project
    Added standard dependencies for working with spring + hibernate,
    working with postgres. Also integration tests with testcontainers.
    open-in-view is disabled at the initial stages of the project
    to avoid performance issues in the future.
    
    Jira ID: FIN-30
    Testing done: ran new tests, local dev runs

---
+++ src/main/java/ru/finess/finess/FinessApplication.java
@@ -0,0 +1,13 @@
+package ru.finess.finess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinessApplication.class, args);
+    }
+
+}
