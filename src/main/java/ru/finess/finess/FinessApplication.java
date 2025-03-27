a54f2423756f4737dc23d4f7ba4e92a158130799
VlaLee <vladivektin@mail.ru>
2025-03-27 14:03:46 +0300

    Added formatter and test coverage tool
    Added spotless for formatting code and jacoco for test coverage, integrated in build stage
    
    Jira ID: FIN-31
    Testing done: Manually

--- src/main/java/ru/finess/finess/FinessApplication.java
+++ src/main/java/ru/finess/finess/FinessApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FinessApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(FinessApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(FinessApplication.class, args);
+  }
 }
